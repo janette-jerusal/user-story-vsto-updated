46cea39242d2943c06e30985f4ea2eccf98c1382
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-17 13:00:42 -0400

    Update ThisWorkbook.cs

--- ThisWorkbook.cs
+++ ThisWorkbook.cs
@@ -1,41 +1,14 @@
-using System;
-using System.Data;
-using System.Drawing;
-using System.Windows.Forms;
-using Microsoft.VisualStudio.Tools.Applications.Runtime;
-using Excel = Microsoft.Office.Interop.Excel;
-using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Core;
 
 namespace Test2
 {
     public partial class ThisWorkbook
     {
-        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
-            return new Ribbon1();
+            return new Ribbon1(); // Match your class name
         }
-
-        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
-        {
-        }
-
-        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
-        {
-        }
-
-        #region VSTO Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InternalStartup()
-        {
-            this.Startup += new System.EventHandler(ThisWorkbook_Startup);
-            this.Shutdown += new System.EventHandler(ThisWorkbook_Shutdown);
-        }
-
-        #endregion
-
     }
 }
+
