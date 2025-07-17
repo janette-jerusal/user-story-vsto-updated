c2cd53e429339cf5e8259f04e84fadccc172a274
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-17 16:08:47 -0400

    Update ThisWorkbook.cs

--- ThisWorkbook.cs
+++ ThisWorkbook.cs
@@ -1,27 +1,10 @@
-using Microsoft.Office.Tools.Excel;
-using Microsoft.Office.Core;
-
 namespace Test2
 {
     public partial class ThisWorkbook
     {
-        protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
             return new Ribbon1();
-        }
-
-        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
-        {
-        }
-
-        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
-        {
-        }
-
-        private void InternalStartup()
-        {
-            this.Startup += new System.EventHandler(ThisWorkbook_Startup);
-            this.Shutdown += new System.EventHandler(ThisWorkbook_Shutdown);
         }
     }
 }
