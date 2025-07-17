d82af2d4fa98300788d12902514c131f6d63a08a
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-17 13:00:11 -0400

    Update Ribbon1.cs

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -1,83 +1,35 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using Office = Microsoft.Office.Core;
-
-// TODO:  Follow these steps to enable the Ribbon (XML) item:
-
-// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
-
-//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
-//  {
-//      return new Ribbon1();
-//  }
-
-// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
-//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
-//    move your code from the event handlers to the callback methods and modify the code to work with the
-//    Ribbon extensibility (RibbonX) programming model.
-
-// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
-
-// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
-
+using System.Windows.Forms;
+using Microsoft.Office.Core;
 
 namespace Test2
 {
-    [ComVisible(true)]
-    public class Ribbon1 : Office.IRibbonExtensibility
+    public class Ribbon1 : IRibbonExtensibility
     {
-        private Office.IRibbonUI ribbon;
-
-        public Ribbon1()
-        {
-        }
-
-        #region IRibbonExtensibility Members
+        private IRibbonUI ribbon;
 
         public string GetCustomUI(string ribbonID)
         {
-            return GetResourceText("Test2.Ribbon1.xml");
+            var resourceName = "Test2.Ribbon1.xml"; // matches your project + file name
+
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
 
-        #endregion
-
-        #region Ribbon Callbacks
-        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
-
-        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        public void Ribbon_Load(IRibbonUI ribbonUI)
         {
             this.ribbon = ribbonUI;
         }
 
-        #endregion
-
-        #region Helpers
-
-        private static string GetResourceText(string resourceName)
+        public void OnCompareClick(IRibbonControl control)
         {
-            Assembly asm = Assembly.GetExecutingAssembly();
-            string[] resourceNames = asm.GetManifestResourceNames();
-            for (int i = 0; i < resourceNames.Length; ++i)
-            {
-                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
-                {
-                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
-                    {
-                        if (resourceReader != null)
-                        {
-                            return resourceReader.ReadToEnd();
-                        }
-                    }
-                }
-            }
-            return null;
+            MessageBox.Show("Compare logic goes here!");
         }
-
-        #endregion
     }
 }
+
